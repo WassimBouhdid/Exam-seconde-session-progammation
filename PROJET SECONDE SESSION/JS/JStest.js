9984adf1f80b9943bb3c3aa4f6fc032e5d4b8742
WassimBouhdid <64103126+WassimBouhdid@users.noreply.github.com>
2020-07-30 11:49:05 +0200

    Add files via upload

--- PROJET SECONDE SESSION/JS/JStest.js
+++ PROJET SECONDE SESSION/JS/JStest.js
@@ -13,13 +13,7 @@ const PECHE=3.99
 const ABRICOT=5.95
 const ORANGE=1.95
 
-// création d'un arrayavec tout les fruits
-
-/*let prix=[BANANE,MANGUE,ANANAS,POIRE,POMME,RAISIN,FRAISE,PECHE,ABRICOT,ORANGE]
-
-let fruits=[banane,mangue,ananas,poire,pomme,raisin,fraise,pêche,abricot,orange]
-*/ 
-
+// création d'un array contenant les livraisons créé par la fonctions envoyerFormulaire()
 
 
 let identifiant = 0;
@@ -27,10 +21,12 @@ let nom;
 let prénom;
 let produit;
 let quantité;
-let prix;
+let prix=0;
 let adresse;
 let mail;
 let messageSupplémentaire;
+
+// création des variables qui contiendront les objets utilisés ci-dessous
 
 let objectFruits={
     Banane :BANANE,
@@ -56,82 +52,133 @@ let objectFormulaire={
     MessageSupplémentaire : undefined,
 };
 
+// création d'un array qui va contenir les livraison
+
+let arrayLivraison=[]
+
+
 /* cette fonction me permet de récuperer la value de chaqu'un des champs et de les insérer dans un object */
 
 function envoyerFormulaire(){
 
-    identifiant++;
-
-    let objectFormulaire={
-        Nom : undefined,
-        Prénom : undefined,
-        Produit : undefined,
-        Quantité : undefined,
-        Prix : undefined,
-        Adresse : undefined,
-        Mail : undefined,
-        MessageSupplémentaire : undefined,
-    };
-
-    nom=document.getElementById("nomInput").value
+    nom=document.getElementById("nomInput").value;
         //console.log(nom)
-    prénom=document.getElementById("prénomInput").value
+    prénom=document.getElementById("prénomInput").value;
         // console.log(prénom)
-    produit=document.getElementById("prodInput").value
-        //console.log(produit)
-    quantité=document.getElementById("quantInput").value
-        //console.log(quantité)
-    prix=document.getElementById("prixInput").value
-        //console.log(prix)
-    adresse=document.getElementById("adresseInput").value
-        //console.log(adresse)
-    mail=document.getElementById("mailInput").value
-        //console.log(mail)
-    messageSupplémentaire=document.getElementById("messageSuppInput").value
-        //console.log(messageSupplémentaire)
-        //alert("test")  
+    produit=document.getElementById("prodInput").value;
+       // console.log(produit);
+       // console.log(typeof produit); 
+    quantité=document.getElementById("quantInput").value;
 
-        objectFormulaire.Nom=nom
-        objectFormulaire.Prénom=prénom
-        objectFormulaire.Produit=produit
-        objectFormulaire.Quantité=quantité
-        objectFormulaire.Prix=prix+" €"
-        objectFormulaire.Adresse=adresse
-        objectFormulaire.Mail=mail
-        objectFormulaire.MessageSupplémentaire=messageSupplémentaire
-
-    let indice=Object.keys(objectFormulaire)
-
-    let ligne="";
-
-    var tr = "<tr id=" + identifiant + ">";
-    
-    for ( let i=0; i<indice.length;i++) {
-        tr += "<td>"+objectFormulaire[indice[i]]+"</td>";
+    if(document.getElementById("prodInput").value=="pomme"){
+        prix=POMME*Number(quantité)
     }
-    tr+="</tr>"
-    ligne+=tr
+    else if(document.getElementById("prodInput").value=="poire"){
+        prix=POIRE*Number(quantité)
+    }
+    else if(document.getElementById("prodInput").value=="fraise"){
+        prix=FRAISE*Number(quantité)
+    }
+    else if(document.getElementById("prodInput").value=="pêche"){
+        prix=PECHE*Number(quantité)
+    }
+    else if(document.getElementById("prodInput").value=="abricot"){
+        prix=ABRICOT*Number(quantité)
+    }
+    else if(document.getElementById("prodInput").value=="banane"){
+        prix=BANANE*Number(quantité)
+    }
+    else if(document.getElementById("prodInput").value=="raisin"){
+        prix=RAISIN*Number(quantité)
+    }
+    else if(document.getElementById("prodInput").value=="ananas"){
+        prix=ANANAS*Number(quantité)
+    }
+    else if(document.getElementById("prodInput").value=="mangue"){
+        prix=MANGUE*Number(quantité)
+    }
+    else if(document.getElementById("prodInput").value=="orange"){
+        prix=ORANGE*Number(quantité)
+    }
 
-    document.getElementById("tableau").innerHTML+=ligne
+    prix=prix.toFixed(2)
 
+        // console.log(prix)
+        // console.log(Number(quantité)*BANANE)
+    adresse=document.getElementById("adresseInput").value;
+        //console.log(adresse)
+    mail=document.getElementById("mailInput").value;
+        //console.log(mail)
+    messageSupplémentaire=document.getElementById("messageSuppInput").value;
+        
+        if(nom=="" || prénom=="" || produit=="" || quantité=="" || adresse=="" || mail=="" ){
+
+            alert("veuillez complétez tout les champs !!");
+
+        }
+        else{
+
+            identifiant++;
+
+            //console.log(messageSupplémentaire)
+            //alert("test")  
+
+            objectFormulaire.Nom=nom;
+            objectFormulaire.Prénom=prénom;
+            objectFormulaire.Produit=produit;
+            objectFormulaire.Quantité=quantité;
+            objectFormulaire.Prix=prix+" €";
+            objectFormulaire.Adresse=adresse;
+            objectFormulaire.Mail=mail;
+            objectFormulaire.MessageSupplémentaire=messageSupplémentaire;
+
+            arrayLivraison.push(objectFormulaire) // insertion de l'object créé via le formulaire dans l'array pour permettre plus tard a une autre fonction et a un boutton de pouvoir supprimer deslivraison
+
+            let indice=Object.keys(objectFormulaire);
+
+            let ligne="";
+
+            var tr = "<tr id=" + identifiant + ">";
+            
+            for ( let i=0; i<indice.length;i++) {
+
+                tr += "<td>"+objectFormulaire[indice[i]]+"</td>";
+
+            }
+
+            tr+="<td><button id="+"supprimer"+" type="+"button"+" onclick="+supprimerCommande()+">supprimer</button></td></tr>";
+            ligne+=tr;
+
+            document.getElementById("corpsTableau").innerHTML+=ligne;
+
+        }
 }
+
+function supprimerCommande (){
+    let patate
+}
+
+
+/* TERMINER !!! il reste plus qu'à corriger les erreurs potentielles */
+
+//  FONCTION LISTE DES PRIX
+
 
 function listerPrix(){
 
-    let tableau="<table id="+"'tableauPrix'"+"><thead><th id="+"'fruit'"+">Fruit</th><th id="+prix+">Prix</th></thead><tbody id="+"'corpsTableauPrix'"+">"
+    let tableau="<table id="+"'tableauPrix'"+"><thead><th id="+"'fruit'"+">Fruit</th><th id="+prix+">Prix</th></thead><tbody id="+"'corpsTableauPrix'"+">";
 
-    let indice=Object.keys(objectFruits)
+    let indice=Object.keys(objectFruits);
 
     for(let f=0;f<indice.length;f++){
-        tableau+="<tr>"
-        tableau+="<td>"+indice[f]+"</td><td>"+objectFruits[indice[f]]+" €"+"</td>"
-        tableau+="</tr>"
+
+        tableau+="<tr>";
+        tableau+="<td>"+indice[f]+"</td><td>"+objectFruits[indice[f]]+" €"+"</td>";
+        tableau+="</tr>";
+
     }
 
-    tableau+="</tbody></table>"
-    document.getElementById("listePrix").innerHTML+=tableau
-}
+    tableau+="</tbody></table>";
+    document.getElementById("listePrix").innerHTML+=tableau;
 
-// alert("test")
-// alert("test")
-// alert("test")
+}
